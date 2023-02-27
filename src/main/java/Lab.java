68c1b9fa115e9238845de5e1ca9afc115a34d8a6
supriyaraul <supriyaraul@gmail.com>
2023-02-27 02:50:37 +0000

    Boolean_Operators

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,16 +14,20 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+        return b;
+        //if (b==true)
+        //return true;
+       // else{
+        //return false;
+    //}
     }
-
     /**
      * @param b a true/false value.
      * @return if b is true, return false. if b is false, return true.
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return !b;
     }
 
     /**
@@ -33,7 +37,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        return b1 && b2;
     }
 
     /**
@@ -43,7 +47,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        return b1 || b2;
     }
 
 
